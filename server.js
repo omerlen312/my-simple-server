52ff08bd9cbd007ed761217bc64967b199ebb2f6
omerlen312 <omerlen1000@gmail.com>
2025-06-11 14:19:29 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not set
+
+// Serve static files from the 'public' directory
+app.use(express.static("public"));
+
+//route for the home page (index.html)
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// route for the about page (about.html)
+app.get("/about", (request, response) => {
+  response.send("about is mid...");
+});
+
+//start the server
+app.listen(PORT, () => {
+  console.log(`server listening at port:${PORT}`);
+});
